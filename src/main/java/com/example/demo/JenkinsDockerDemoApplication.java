4fb68cba4e39ea53051cdf9dc01812d3c989b5e6
zhanpengguo <18396815481@163.com>
2021-07-16 15:08:23 +0800

    打印日志

--- src/main/java/com/example/demo/JenkinsDockerDemoApplication.java
+++ src/main/java/com/example/demo/JenkinsDockerDemoApplication.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @SpringBootApplication
 public class JenkinsDockerDemoApplication {
@@ -15,6 +17,20 @@ public class JenkinsDockerDemoApplication {
 
     @GetMapping
     public String hello() {
+        print();
         return "hello 妹妹";
+    }
+
+    private void print() {
+        new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                log.info("打印数字：{}", i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
